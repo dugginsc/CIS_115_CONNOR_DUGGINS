6285b4b8bcd8b6507013d94823cbd84f4ec35b80
SANDHILLS\lh218-03 <jad.talbert@gamil.com>
2025-08-27 09:53:44 -0400

    calc was made

---
+++ sumNumbers.py
@@ -0,0 +1,14 @@
+#This program sums two numbers
+
+num1 = input("Please enter the first number: ")
+num2 = input("Please enter the second number: ")
+sum = float(num1) + float(num2)
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+
+(num3) = input("Please enter your third number: ")
+(num4) = input("Please enter your fourth number: ")
+diff = float(num4) - float(num3)
+
+print(diff)
